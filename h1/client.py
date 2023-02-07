277b854d0b8a648582ca24acdd70e2d66d99f284
zmweske <zmweske@crimson.ua.edu>
2023-02-07 03:00:19 -0600

    final

--- h1/client.py
+++ h1/client.py
@@ -20,90 +20,95 @@ import socket
 import time
 import argparse
 import uuid
+import json
+import threading
+import traceback
 
 SERVER_ADDR = "localhost"
 SERVER_PORT = 8888
-USER = str(uuid.uuid4())
 
+class chat_client():
+    def __init__(self, SERVER_ADDR, SERVER_PORT, CUSTOM_PORT, UUID, USER):
+        try:
+            self.STOP_COMMAND = False
+            self.UUID = UUID
+            self.USER = USER
+            
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.connect((SERVER_ADDR, SERVER_PORT))
+            # client_socket.send(b"CONNECT " + UUID.encode())
+            message = ("CONNECT " + json.dumps({"user": self.USER, "uuid": self.UUID, "action": "CONNECT", "message": "has joined"})).encode()
+            client_socket.send(message)
+            new_port = int(client_socket.recv(1024).decode())
+            client_socket.close()
+            print("Attempting to begin new connection on port", new_port)
 
-def connection(chat, port):
-    if chat == "QUIT":
-        return None
+            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            time.sleep(0.2)
+            if(CUSTOM_PORT != 0):
+                print(CUSTOM_PORT)
+                new_port = CUSTOM_PORT
+            self.client_socket.connect((SERVER_ADDR, new_port))
+            
+            printing_thread = threading.Thread(target=self.monitor_socket)
+            printing_thread.start()
+            input_thread = threading.Thread(target=self.monitor_input)
+            input_thread.start()
+            
+            input_thread.join()
+            self.STOP_COMMAND = True
+            
+            self.client_socket.close()
+            # self.client_socket.shutdown(socket.SHUT_WR)
+            # self.client_socket.setblocking(0)
+            printing_thread.join()
 
-    try:
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect((SERVER_ADDR, port))
-        client_socket.send((chat).encode())
-        data = client_socket.recv(1024).decode()
-        client_socket.close()
-        return(data)
-    except Exception as e:
-        print("Client error: " + type(e).__name__)
+        except Exception as e:
+            print("Client error: " + type(e).__name__)
+            # traceback.print_exc()
+        
+        
+    def monitor_socket(self):
+        while not self.STOP_COMMAND:
+            try:
+                data = self.client_socket.recv(1024).decode()
+                if data == '':
+                    self.STOP_COMMAND = True
+                    print("Connection closed by server, press enter to quit...")
+                    continue
+                print({True: "No data received", False: data}[data == ''])  # '\r' + '\033[K' + 
+                if not self.STOP_COMMAND:
+                    print("> ")
+            # except socket.timeout:
+            #     continue
+            except Exception as e:
+                print(type(e).__name__ + ' ' + str(e))
+                self.STOP_COMMAND = True
+                continue
+            
+    def monitor_input(self):
+        while not self.STOP_COMMAND:
+            try:
+                chat = input("> ")
+            except EOFError as e:
+                chat = '\x04'
+            if chat.strip() == "":
+                continue
+            self.client_socket.send((chat).encode())
+            if chat == '\x04' or chat == "QUIT":
+                print("Exiting client...")
+                self.STOP_COMMAND = True
+                continue
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    UUID = str(uuid.uuid4())
+
     parser.add_argument("-a", "--address", action="store", dest="SERVER_ADDR", default=SERVER_ADDR)
     parser.add_argument("-p", "--port",    action="store", dest="SERVER_PORT", default=SERVER_PORT)
-    parser.add_argument("-u", "--user", "--uuid", action="store", dest="USER", default=USER)
+    parser.add_argument("-u", "--user", "--username", "--name", action="store", dest="USER", default=UUID)
+    parser.add_argument("-c", "--custom_port", action="store", dest="CUSTOM_PORT", default=0)
     args = parser.parse_args()
-    
-    SERVER_ADDR = args.SERVER_ADDR
-    SERVER_PORT = int(args.SERVER_PORT)
-    USER = args.USER
 
-    # print(SERVER_ADDR, SERVER_PORT, USER)
-    # try:
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((SERVER_ADDR, SERVER_PORT))
-    client_socket.send(b"CONNECT " + USER.encode())
-    # client_socket.send(("GET " + page + " HTTP/1.0").encode())
-    new_port = int(client_socket.recv(1024).decode())
-    client_socket.close()
-    print("Attempting to begin new connection on port", new_port)
-
-    stop = False
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # client_socket.setblocking(0)
-    time.sleep(0.2)
-    client_socket.connect((SERVER_ADDR, new_port))
-    while not stop:
-        # while (chat := input("> ")) == '':
-        #     # print("please send a chat...")
-        #     pass
-        try:
-            chat = input("> ")
-        except EOFError as e:
-            chat = '\x04'
-        if chat.strip() == "":
-            continue
-        # TODO make input non blocking
-        client_socket.send((chat).encode())
-        data = client_socket.recv(1024).decode()
-        print({True: "No data received", False: data}[data == None])
-        if chat == '\x04' or chat == "QUIT":
-            stop = True
-            print("Exiting client...")
-            continue
-    client_socket.close()
-    # except Exception as e:
-    #     print("Client error: " + type(e).__name__)
-
-
-    # data = None
-    # stop = False
-    # while not stop:
-    #     if len(sys.argv) != 4:
-    #         page = input("\nEnter a localhost:8888 page to view: ")
-    #         if page == '\x04' or page == "quit":
-    #             stop = True
-    #             print("Exiting client...")
-    #             continue
-    #     else:
-    #         page = sys.argv[3]
-    #         stop = True
-    #     data = connection(page)
-    #     print({True: "No data received", False: data}[data == None])
-
-
-
-    
+    client_connection_obj = chat_client(args.SERVER_ADDR, int(args.SERVER_PORT), int(args.CUSTOM_PORT), UUID, args.USER)
--- h1/server.py
+++ h1/server.py
@@ -21,33 +21,35 @@ import json
 import logging
 from collections import defaultdict
 
-# FIXME log all print statements
-
 SERVER_PORT = 8888
 THREADS = queue.Queue()
 MESSAGES = queue.Queue()  # send messages from threads to main to be broadcasted
 ACTIVE_USERS = defaultdict(lambda: queue.Queue())  # send broadcasts back to threads
+ACTIVE_USERNAMES = {}
+LOGGING_OUT = queue.Queue()
 
 STOP_COMMAND = False
 PORT_RANGE = [9001, 9999]
 
 
 class connection_thread():
-    def __init__(self, port, user, message_sender, incoming_messages):
+    def __init__(self, port, uuid, user, message_sender, incoming_messages):
         self.port = port
         self.user = user
+        self.uuid = uuid
         self.messages_broadcaster = message_sender
         self.incoming = incoming_messages
         self.outgoing = queue.Queue()
         self.stop = False
 
-        logging.info("Starting client listener thread on port " + str(port))  # FIXME log all print statements
+        logging.info("Starting client listener thread on port " + str(self.port))
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_socket.bind(("localhost", port))
         self.server_socket.listen(20)
         self.client_socket, addr = self.server_socket.accept()
-        logging.info("User connect on port " + str(port) + ": " + self.user)
+        logging.info("User connect on port " + str(self.port) + ": " + self.uuid + 
+                     {False: "(" + user + ")", True: "(without username)"}[user==uuid])
                 
         self.broadcast_listener = threading.Thread(target=self.listen_to_broadcast)
         self.client_listener = threading.Thread(target=self.accept_message)
@@ -56,89 +58,114 @@ class connection_thread():
         self.client_listener.start()
         self.broadcast_listener.start()
         self.client_sender.start()
-
+        
+        self.client_listener.join()
+        self.stop = True
+        self.broadcast_listener.join()
+        try:  # client socket may already be closed, but
+              #   this will prevent a blocking client thread
+            self.client_socket.send(b' ')
+        except BrokenPipeError as e:
+            pass
+        self.client_sender.join()
+        logging.info("Logging out user " + self.uuid + {False: "(" + self.user + ")", True: ""}[self.user==self.uuid])
+        self.messages_broadcaster.put(json.dumps({"user": self.user, "uuid": self.uuid, "action": "QUIT", "message": "Logged out"}))
+        LOGGING_OUT.put(self.uuid)
+        logging.info("Closing server connection " + str(self.port) + "...")
+        self.client_socket.close()
 
     def send_to_client(self):
         while not self.stop:
             to_send = self.outgoing.get()
-            self.client_socket.sendall(to_send)
+            if self.stop:
+                continue
+            try:
+                self.client_socket.sendall(to_send)
+            except BrokenPipeError as e:
+                self.stop = True
+            except Exception as e:
+                logging.warning("80: " + type(e).__name__ + ' ' + str(e))
         
     def listen_to_broadcast(self):
-        # print("listenening to broadcaster")
         while not self.stop:
             json_msg = self.incoming.get()
-            # print("---\n", json_msg, "\n---")
-            if json_msg["user"] == self.user:
+            if self.stop:
                 continue
-            message = json_msg["user"] + ": " + json_msg["message"]
-            # print("sending " + message + " to client: " + self.user)
+            if json_msg["uuid"] == self.uuid:
+                continue
+            message = json_msg["user"] + {False:": ",True:" "}[json_msg["action"]=="CONNECT"] + json_msg["message"]
             message = message.encode()
             self.outgoing.put(message)
-            # self.client_socket.sendall(message)  # FIXME
             
     def accept_message(self):
-        response = b""
         try:
             while not self.stop:
+                response = b""
                 try:
                     message = self.client_socket.recv(1024).decode()
                     try:
                         if message == "QUIT" or message == '\x04':
                             response = b"Closing server connection...goodbye!"
-                            self.stop = True
-                        elif message == "LIST":
-                            response = ('\n'.join(list(ACTIVE_USERS.keys()))).encode()
-                        else:
-                            message = json.dumps({"user": self.user, "message": message})
+                            message = json.dumps({"user": self.user, "uuid": self.uuid, "action": "QUIT", "message": "Logged off"})
                             message = message.encode()
                             self.messages_broadcaster.put(message)
-                            response = b"echo:" + message
+                            self.stop = True
+                        elif message == "LIST":
+                            # response = ('\n'.join(map(lambda u: u[u.find(':')+1:], list(ACTIVE_USERS.keys())))).encode()
+                            response = ('\n'.join(list(ACTIVE_USERNAMES.values()))).encode()
+                            self.outgoing.put(response)
+                        elif message != "":
+                            message = json.dumps({"user": self.user, "uuid": self.uuid, "action": "message", "message": message})
+                            message = message.encode()
+                            self.messages_broadcaster.put(message)
+                            # response = b"echo:" + message  # only used for server to client echo TODO REMOVE
+                            # self.outgoing.put(response)    # only used for server to client echo TODO REMOVE
+                    except BrokenPipeError as e:
+                        self.stop = True
                     except Exception as e:
-                        logging.error(" - Error: " + type(e).__name__ + str(e))
-
-                    self.outgoing.put(response)
-                    # self.client_socket.sendall(response)
-
-                    # VERSION 1:
-                    #   for byte in response:
-                    #       client_socket.send(byte.to_bytes(1, "little"))
-                    # VERSION 2:
-                    #   client_socket.send(response)
-                    # VERSION 3:
-                    #   client_socket.sendall(response)
+                        logging.error("118: " + type(e).__name__ + ' ' + str(e))
 
                 except Exception as e:
-                    logging.error("Error: " + type(e).__name__ + str(e))
+                    logging.error("124: " + type(e).__name__ + ' ' + str(e))
                     break
             time.sleep(1)
-            logging.info("Logging out user " + self.user)
-            ACTIVE_USERS[self.user] = "QUIT"
-            logging.info("Closing server connection " + str(self.port) + "...")
-            self.client_socket.close()
+            self.stop = True
+            self.incoming.put(" ")
+            self.outgoing.put(" ")
         except Exception as e:
-            logging.error("Error: " + type(e).__name__ + str(e))
+            logging.error("130: " + type(e).__name__ + ' ' + str(e))
 
 
 
 
 
 def broadcast_listener():
-    to_delete = queue.Queue()
     while not STOP_COMMAND:
         message = MESSAGES.get()
         json_msg = json.loads(message)
-        logging.info(json_msg["user"] + ": " + json_msg["message"])
+        uuid = json_msg["uuid"]
+        user = json_msg["user"]
         
-        for user in ACTIVE_USERS:
-            if not isinstance(ACTIVE_USERS[user], queue.Queue):
-                to_delete.put_nowait(user)
+        logging.info(uuid + {False: "(" + user + ")", True: ""}[user==uuid] + {False:": ",True:" "}[json_msg["action"]=="CONNECT"] + json_msg["message"])
+        
+        for i in ACTIVE_USERS:
+            if i not in ACTIVE_USERNAMES:
                 continue
-            if json_msg["user"] != user:
-                ACTIVE_USERS[user].put_nowait(json_msg)
-        while not to_delete.empty():
-            user = to_delete.get_nowait()
-            logging.info("user " + user + " logged out...")
-            del ACTIVE_USERS[user]
+            if uuid != i:
+                if i in ACTIVE_USERS and isinstance(ACTIVE_USERS[i], queue.Queue):
+                    try:
+                        ACTIVE_USERS[i].put(json_msg)
+                    except BrokenPipeError as e:
+                        LOGGING_OUT.put(i)
+        while not LOGGING_OUT.empty():
+            temp = LOGGING_OUT.get_nowait()
+            # logging.info("user " + temp + (("") if (temp not in ACTIVE_USERNAMES or ACTIVE_USERNAMES[temp] == temp) 
+            #              else "(" + ACTIVE_USERNAMES[temp] + ")") + " logged out...")
+            if temp in ACTIVE_USERS:
+                del ACTIVE_USERS[temp]
+            if temp in ACTIVE_USERNAMES:
+                del ACTIVE_USERNAMES[temp]
+                
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -146,12 +173,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
     SERVER_PORT = int(args.SERVER_PORT)
 
-    # logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
     logging.basicConfig(filename='h1/server.log', encoding='utf-8', 
                         level=logging.DEBUG, filemode='w',
                         format='%(asctime)s - %(levelname)-7s - %(message)s')
     logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
-
 
     # Prepare initial sever socket
     logging.info("Starting server on port " +  str(SERVER_PORT))
@@ -172,14 +197,21 @@ if __name__ == "__main__":
         if not (message := client_socket.recv(1024).decode()).startswith("CONNECT"):
             client_socket.close()
             continue
-        user = message[8:]
-        # ACTIVE_USERS[user] = True
+        message = message[8:]
+        try:
+            json_msg = json.loads(message)
+        except:
+            logging.warning("Error parsing connection json request: " + str(message))
+            continue
+        uuid = json_msg["uuid"]
+        user = json_msg["user"]
+        ACTIVE_USERNAMES[uuid] = user
         client_port = random.randrange(PORT_RANGE[0], PORT_RANGE[1])
-        client_socket.send(str(client_port).encode())
-        # client_socket.send(client_port)
+        client_socket.sendall(str(client_port).encode())
         client_socket.close()
-        new_thread = threading.Thread(target=connection_thread, args=(client_port,user,MESSAGES,ACTIVE_USERS[user]))
+        new_thread = threading.Thread(target=connection_thread, args=(client_port,uuid,user,MESSAGES,ACTIVE_USERS[uuid]))
         new_thread.start()
+        MESSAGES.put(message)
         THREADS.put_nowait(new_thread)
         # new_thread.join()
 
